d4304452548f4a6eed376404a8bd08622f13861c
javier ferrales <ferralespereira@gmail.com>
2021-11-05 18:47:20 -0400

    creting first method and using ORM

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,11 +4,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use App\Entity\Video;
+
 
 class UserController extends AbstractController
 {
     public function index(): Response
     {
+
+        $user_repo = $this->getDoctrine()->getRepository(User::Class);
+        $video_repo = $this->getDoctrine()->getRepository(Video::Class);
+
+        $users = $user_repo->findAll();
+
+        foreach($users as $user){
+            echo "<h1>{$user->getName()}  {$user->getSurname()}</h1>";
+
+            foreach($user->getVideos() as $video){
+                echo "<p>{$video->getTitle()} - {$video->getUser()->getEmail()}</p>";
+            }
+        }
+
+        die();
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/UserController.php',
