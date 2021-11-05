54da72b7b6164de80113b6ee7a030a466a9379ce
javier ferrales <ferralespereira@gmail.com>
2021-11-05 16:52:26 -0400

    more

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -5,13 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class UserController extends AbstractController
+class VideoController extends AbstractController
 {
     public function index(): Response
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
+            'path' => 'src/Controller/VideoController.php',
         ]);
     }
 }
