821e9ef69d14478990615f318456a8f9857823fb
javier ferrales <ferralespereira@gmail.com>
2021-11-09 19:03:33 -0500

    sending json data and getting response

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Entity\User;
 use App\Entity\Video;
 
@@ -62,5 +64,41 @@ class UserController extends AbstractController
             'users' => $users,
             'videos' => $videos
         ]);
+    }
+
+    public function create(Request $request){
+
+        // Recoger los datos por post
+        $json = $request->get('json', null);
+
+        // Decodificar el json
+        $params = json_decode($json);
+        // var_dump($params);
+        // die();
+
+        // Hacer una respuesta por defecto
+        $data = [
+            'status' => 'error',
+            'code'   => 200,
+            'message'=> 'El usuario nos se ha creado',
+            'params' => $params,
+            'json' => $json
+        ];
+        // 'json'   => $json,
+
+        // Comprobar y validar datos
+
+        // Si la validacion es correcta, crear el objeto del usuario
+
+        // Cifrar la contrasena
+
+        // Comprobar si el usuario existe
+
+        // Si no existe guardarlo en la base de datos
+
+        // Hacer respuesta en json
+        // return $this->resjson($data);
+        // return $this->json($data);
+        return new JsonResponse($data);
     }
 }
