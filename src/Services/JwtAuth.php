b3491853a46ca1688407104cca108f875accf9fa
javier ferrales <ferralespereira@gmail.com>
2022-04-21 16:34:19 -0400

    jwt

--- src/Services/JwtAuth.php
+++ src/Services/JwtAuth.php
@@ -11,7 +11,7 @@ class JwtAuth{
 
     public function __construct($manager){
         $this->manager = $manager;  
-        $this->key = 'este_es_la_key_del_token_hjknxnlzqlmx56565@@%%njm';
+        $this->key = 'here-write-some-private-key-that-only-you-know';
     }
     
     public function signup($email, $pwd, $gettoken = null){
